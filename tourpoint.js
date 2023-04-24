cad6b404191c46270dcb6024cc592ef1b23c9c91
aomkul <124546386+aomkul@users.noreply.github.com>
2023-04-24 11:49:09 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสัตว์เชียงใหม่",
+          "type": "natural",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.9439795531805,
+            18.804383768665105
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ต๋องเต็มโต๊ะ",
+          "type": "special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.96771965797535,
+            18.79663462646276
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานดาราศาสตร์",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.95769063499858,
+            18.852674248623202
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนพฤกษศาสตร์สมเด็จพระนางเจ้าสิริกิติ์",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.86060008774234,
+            18.890268296088692
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระธาตุชุ่มเมือง",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.90072464517965,
+            18.921091867145577
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระธาตุดอยสุเทพ",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            98.92213251852382,
+            18.804920719270967
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดแสนเมืองมาหลวง",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.98634697019338,
+            18.793993662986026
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อ่างแก้ว",
+          "type": "natural",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.94990215448098,
+            18.807071285936814
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "คลองแม่ข่า",
+          "type": "special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.99860867472086,
+            18.781081901418304
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "โป่งแยง จังเกิ้ล",
+          "type": "natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.94053892326588,
+            18.870519781717576
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
